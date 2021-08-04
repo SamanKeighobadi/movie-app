c30f1190659eecb898c87ff6d517fb781100d503
saman keighobadi <saman.keighobadi@gmail.com>
2021-08-04 14:08:24 +0430

    add footer

--- src/Components/common/Footer.jsx
+++ src/Components/common/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='text-white h-10 flex justify-center items-center bg-gray-800 '>
+            <h1 className='text-lg'>saman.keighobadi@gmail.com</h1>
         </div>
     );
 };
